28b32c200a8762c4be559837771f9b59d5efd844
Karen Nason <knason4@gmail.com>
2022-10-27 21:05:15 -0500

    make todo, strike when done, button to remove

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //Part 1 - make a todo, strike through finished todos, be able to remove todos.
+
 const form = document.querySelector('#todo-form');
-//const todoInput = document.querySelector('#todo-item');
+const todoInput = document.querySelector('#todo-item');
 const todoList   = document.querySelector('#todo-list');
 
-const storeTodo = JSON.parse(localStorage.getItem('todo')) || [];
-
+//make buttons removable and strike through text
 todoList.addEventListener('click', function (event) {
     if(event.target.tagName === 'BUTTON'){
         event.target.parentElement.remove();
@@ -19,18 +19,14 @@ todoList.addEventListener('click', function (event) {
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    //console.log(todoInput.value);
     let newTodo = document.createElement('li');
     let removeButton = document.createElement('button');
+   
     
     removeButton.innerText = 'Done!'
-    newTodo.innerText = document.getElementById("todo-item").value;
+    newTodo.innerText = todoInput.value;
     newTodo.appendChild(removeButton);
     todoList.appendChild(newTodo);
-    // todoInput.value = '';
-    form.reset();
-})
-
-
-
-
+    todoInput.value = '';
+    
+});
